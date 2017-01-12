89ab38107722f0687a205caf320fe3f2dd983911
Shanker1234 <shanker.perumalla@gmail.com>
2017-01-12 16:57:04 +0530

    Commit Test from eclipse

---
+++ src/main/java/com/shan/myapp/model/Circle.java
@@ -0,0 +1,21 @@
+package com.shan.myapp.model;
+
+public class Circle {
+    private int Id;
+    private String Name;
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        this.Id = id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        this.Name = name;
+    }
+    
+    
+
+}
